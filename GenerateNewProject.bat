9ad70f84921c041fb55c837bf814834610bb4787
Avnish Kirnalli <avnishkirnalli@gmail.com>
2023-02-26 07:49:22 +0530

    Added Generation Script

---
+++ GenerateNewProject.bat
@@ -0,0 +1,24 @@
+:: Copyright Avnish Kirnalli 2022.
+
+@echo off
+
+@echo off & title Setup DrEngine
+
+goto :DOES_PYTHON_EXIST
+
+:DOES_PYTHON_EXIST
+python -V | find /v "Python" >NUL 2>NUL && (goto :PYTHON_DOES_NOT_EXIST)
+python -V | find "Python"    >NUL 2>NUL && (goto :PYTHON_DOES_EXIST)
+goto :EOF
+
+:PYTHON_DOES_NOT_EXIST
+echo Python is not installed on your system.
+echo Now opening the download URL.
+start "" "https://www.python.org/downloads/windows/"
+goto :EOF
+
+:PYTHON_DOES_EXIST
+python GenerateNewProject.py
+
+:EOF
+PAUSE
---
+++ GenerateNewProject.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+print("Written by AvnishGameDev for ImGuiBorderlessWindow. https://github.com/avnishgamedev/ImGuiBorderlessWindow")
+
+projName = input("Enter project name: ")
+print("Generating project")
+
+os.mkdir(f"../{projName}")
+
+shutil.copytree("ImGuiBorderlessWindow/ImGui", f"../{projName}/ImGuiBorderlessWindow/ImGui")
+shutil.copyfile("ImGuiBorderlessWindow/ImGuiBorderlessWindow.vcxproj", f"../{projName}/ImGuiBorderlessWindow/ImGuiBorderlessWindow.vcxproj")
+
+for basename in os.listdir(f"../ImGuiBorderlessWindow/ImGuiBorderlessWindow"):
+    if basename.endswith('.h'):
+        pathname = os.path.join("../ImGuiBorderlessWindow/ImGuiBorderlessWindow", basename)
+        if os.path.isfile(pathname):
+            shutil.copy2(pathname, f"../{projName}/ImGuiBorderlessWindow/{basename}")
+
+for basename in os.listdir(f"../ImGuiBorderlessWindow/ImGuiBorderlessWindow"):
+    if basename.endswith('.cpp'):
+        pathname = os.path.join("../ImGuiBorderlessWindow/ImGuiBorderlessWindow", basename)
+        if os.path.isfile(pathname):
+            shutil.copy2(pathname, f"../{projName}/ImGuiBorderlessWindow/{basename}")
+
+shutil.copyfile("ImGuiBorderlessWindow.sln", f"../{projName}/ImGuiBorderlessWindow.sln")
+shutil.copyfile("LICENSE", f"../{projName}/LICENSE")
+
+os.chdir(f"../{projName}")
+
+data = ""
+with open("ImGuiBorderlessWindow.sln", "r") as f:
+    data = f.read()
+    data = data.replace("ImGuiBorderlessWindow", projName)
+
+with open("ImGuiBorderlessWindow.sln", "w") as f:
+    f.write(data)
+
+os.rename("ImGuiBorderlessWindow.sln", f"{projName}.sln")
+
+os.chdir("ImGuiBorderlessWindow")
+
+with open("ImGuiBorderlessWindow.vcxproj", "r") as f:
+    data = f.read()
+    data = data.replace("ImGuiBorderlessWindow", projName)
+
+with open("ImGuiBorderlessWindow.vcxproj", "w") as f:
+    f.write(data)
+
+os.rename("ImGuiBorderlessWindow.vcxproj", f"{projName}.vcxproj")
+
+os.chdir("../")
+os.rename("ImGuiBorderlessWindow", projName)
+
+print(f"Project generated successfully in {os.getcwd()}")
